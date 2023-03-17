b33675615f97d6b36b879e84c14cc377124c965d
imamcahayaa <imamcahayaa@students.amikom.ac.id>
2023-03-17 15:37:04 +0700

    melengkapi fungsi main

--- calculator.cs
+++ calculator.cs
@@ -2,9 +2,16 @@ class calculastor
 {
 	static void main(string[] args)
 	{
-	
-	
-	
+		int a = 1;
+		int b = 6;
+		
+		console.writeline("Hasiol penambahan: {0} + {1} = {2}" , a, b, penambahan(a, b));
+		console.writeline("Hasiol pengurangan: {0} - {1} = {2}" , a, b, pengurangan(a, b));
+		
+		console.writeline("\nTekan sembarang key untuk keluar");
+		console.Readkey();
+	 
+		
 	}
 	
 	static int penambahan(int a, int b)
